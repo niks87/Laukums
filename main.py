b2fe680937b2b539d9aa4880e76e181242acf326
niks87 <94444447+niks87@users.noreply.github.com>
2021-11-25 13:25:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ print('K   - ja figūra kvadrāts')
 print('R   - ja figūra riņķis')
 print('____________________')
 
+
+PI = 3.14
+r = float(input("Enter the radius of the circle: "))
+area = PI * r * r
+print("%.2f" %area)
